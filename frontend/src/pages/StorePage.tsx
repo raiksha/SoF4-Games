a7372e038c9143c1a72fa70dfd2435abd87d08c4
Constanza Riquelme <constanza.ri@gmail.com>
2026-06-12 15:03:02 -0400

    fix(frontend): add spinner when loading is true

--- frontend/src/pages/StorePage.tsx
+++ frontend/src/pages/StorePage.tsx
@@ -57,7 +57,6 @@ export default function StorePage() {
                         }}
                     />
                     Cargando juegos...
-                    {/* La animación "spin" está definida en index.css */}
                 </div>
             </main>
         )
